a1930434db1f60c1d9e54d41c5aafc4e09ec2b21
Sujeevraja Sanjeevi <sujeevraja.sanjeevi@sabre.com>
2018-01-30 20:29:26 -0600

    parsed config xml

--- src/com/stochastic/Main.java
+++ src/com/stochastic/Main.java
@@ -1,7 +1,13 @@
 package com.stochastic;
 
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
 
 public class Main {
     /**
@@ -10,7 +16,19 @@ public class Main {
     private final static Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        logger.info("Hello, world!\n");
+        try {
+            File xmlFile = new File("config.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(xmlFile);
+
+            System.out.println("root: " + doc.getDocumentElement().getNodeName());
+
+            Node scenarioNode = doc.getElementsByTagName("scenarioPath").item(0);
+            System.out.println(scenarioNode.getTextContent());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
