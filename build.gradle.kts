1a72ab38ed5831e6693b0066e1f8b2b1107d215f
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2019-05-16 06:58:25 -0700

    added akka dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.typesafe.akka:akka-actor_2.12:2.5.22")
     implementation("org.apache.logging.log4j:log4j-api:2.11.2")
     implementation("org.apache.logging.log4j:log4j-core:2.11.2")
     implementation("org.apache.commons:commons-math3:3.0")
