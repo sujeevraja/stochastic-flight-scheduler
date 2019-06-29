98915e34a1c2b3cd29cf0bbd4ab79d89b89a953c
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2019-06-29 11:12:30 -0700

    parse delays for test for sanity check

--- batch_runner.py
+++ batch_runner.py
@@ -122,7 +122,7 @@ class Controller(object):
     @staticmethod
     def _generate_test_results(orig_cmd):
         cmd = [c for c in orig_cmd]
-        cmd.append("-test")
+        cmd.extend(["-parseDelays", "-test"])
         subprocess.check_call(cmd)
 
     def _run_mean_set(self):
