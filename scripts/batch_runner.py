f73fd93946a044a3ba65ea53146f50aaefbd03f4
Sujeevraja Sanjeevi <sujeevraja26@gmail.com>
2021-05-11 20:19:43 -0700

    add script to build shell script

--- scripts/batch_runner.py
+++ scripts/batch_runner.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import argparse
-import enum
 import logging
 import os
 import subprocess
@@ -10,26 +9,16 @@ import typing
 log = logging.getLogger(__name__)
 
 
-class RunType(enum.Enum):
-    All = enum.auto()
-    Budget = enum.auto()
-    # ExpectedExcess = enum.auto()
-    ColumnCaching = enum.auto()
-    Mean = enum.auto()
-    Parallel = enum.auto()
-    Quality = enum.auto()
-    Scenario = enum.auto()
-    ColumnComparison = enum.auto()
-    CutComparison = enum.auto()
-
-
 class Config(typing.NamedTuple):
     """Script parameters"""
-    run_type: RunType
-    jar_path: str
     cplex_lib_path: str
+    instance: str
+    jar_path: str
+    key: str
+    prefix: str
     path: str
-    names: typing.List[str]
+    quality_run: typing.Optional[str]
+    time_run: typing.List[str]
 
 
 class ScriptException(Exception):
@@ -56,7 +45,8 @@ def clean_delay_files():
             os.remove(os.path.join(sln_path, f))
 
 
-def generate_delays(orig_cmd: typing.List[str], num_scenarios: typing.Optional[int] = None):
+def generate_delays(orig_cmd: typing.List[str],
+                    num_scenarios: typing.Optional[int] = None):
     cmd = [c for c in orig_cmd]
     cmd.append("-generateDelays")
     if num_scenarios is not None:
@@ -64,7 +54,8 @@ def generate_delays(orig_cmd: typing.List[str], num_scenarios: typing.Optional[i
     subprocess.check_call(cmd)
 
 
-def generate_reschedule_solution(orig_cmd: typing.List[str], model: str, prefix: str):
+def generate_reschedule_solution(orig_cmd: typing.List[str], model: str,
+                                 prefix: str):
     cmd = [c for c in orig_cmd]
     cmd.extend([
         "-model", model,
@@ -75,7 +66,8 @@ def generate_reschedule_solution(orig_cmd: typing.List[str], model: str, prefix:
     subprocess.check_call(cmd)
 
 
-def generate_test_results(orig_cmd: typing.List[str], parse_delays: bool, prefix: str):
+def generate_test_results(orig_cmd: typing.List[str], parse_delays: bool,
+                          prefix: str):
     cmd = [c for c in orig_cmd]
     cmd.extend([
         "-type", "test",
@@ -86,17 +78,15 @@ def generate_test_results(orig_cmd: typing.List[str], parse_delays: bool, prefix
     subprocess.check_call(cmd)
 
 
-def generate_training_results(cmd: typing.List[str], models: typing.List[str], prefix: str):
+def generate_all_results(cmd: typing.List[str], models: typing.List[str],
+                         prefix: str):
+    log.info(f"quality run cmd: {cmd}")
     generate_delays(cmd)
     log.info(f"generated delays for {cmd}")
 
     for model in models:
         generate_reschedule_solution(cmd, model, prefix)
         log.info(f"finished training run for {model}")
-
-
-def generate_all_results(cmd: typing.List[str], models: typing.List[str], prefix: str):
-    generate_training_results(cmd, models, prefix)
     generate_test_results(cmd, False, prefix)
     log.info(f"generated test results for {cmd}")
 
@@ -117,9 +107,6 @@ def validate_setup(config: Config):
     log.info("created/checked logs folder")
 
     os.makedirs("solution", exist_ok=True)
-    for f in os.listdir(os.path.join(os.getcwd(), "solution")):
-        if f != '.gitkeep':
-            raise ScriptException("solution folder not empty.")
     log.info("created/checked solution folder")
 
 
@@ -129,15 +116,15 @@ class Controller:
     def __init__(self, config: Config):
         validate_setup(config)
         self.config: Config = config
-        self._models = ["naive", "dep", "benders"]
-        self._defaults = {
-            "-numScenarios", "30",
-            "-r", "0.5",  # reschedule budget fraction
-            "-d", "lnorm",  # delay distribution
-            "-mean", "30",  # delay distribution mean
-            "-c", "first",  # column gen strategy
-            "-f", "hub",  # flight pick strategy
-            "-parallel", "30",  # number of threads for second stage
+        self._models: typing.List[str] = ["naive", "dep", "benders"]
+        self._defaults: typing.Dict[str, str] = {
+            "-numScenarios": "30",
+            "-r": "0.5",  # reschedule budget fraction
+            "-d": "lnorm",  # delay distribution
+            "-mean": "30",  # delay distribution mean
+            "-c": "first",  # column gen strategy
+            "-f": "hub",  # flight pick strategy
+            "-parallel": "30",  # number of threads for second stage
         }
         self._base_cmd = [
             "java",
@@ -152,88 +139,67 @@ class Controller:
         ]
 
     def run(self):
-        run_type = self.config.run_type
-        if run_type == RunType.Budget or run_type == RunType.All:
-            self._run_quality_comparison("budget", "-r",
-                                         ["0.25", "0.5", "0.75", "1", "2"], {})
-        if run_type == RunType.ColumnCaching or run_type == RunType.All:
-            self._run_time_comparison(
-                "column_caching", "-cache", ["y", "n"], {"-parallel": "1"})
-        if run_type == RunType.Mean or run_type == RunType.All:
-            self._run_quality_comparison("mean", "-mean",
-                                         ["15", "30", "45", "60"], {"-d": "exp"})
-        if run_type == RunType.Parallel or run_type == RunType.All:
-            self._run_time_comparison(
-                "thread", "-parallel", ["1", "10", "20", "30"], {})
-        if run_type == RunType.Quality or run_type == RunType.All:
-            self._run_quality_comparison("quality", "-f", ["hub", "rush"], {})
-        if run_type == RunType.Scenario or run_type == RunType.All:
-            self._run_quality_comparison("scenario", "-numScenarios",
-                                         ["10", "20", "30", "40", "50"], {})
-        if run_type == RunType.ColumnComparison or run_type == RunType.All:
-            self._run_time_comparison(
-                "column", "-c", ['enum', 'all', 'best', 'first'], {})
-        if run_type == RunType.CutComparison or run_type == RunType.All:
-            self._run_time_comparison(
-                "cut", "-cut", ["single", "multi"], {"-parallel": "1"})
+        additional_args: typing.Dict[str, str] = {}
+        if self.config.quality_run is not None:
+            if self.config.key == "-m":
+                additional_args["-d"] = "exp"
+            self._quality_run(additional_args)
+        elif self.config.time_run:
+            if self.config.key in ["-cache", "-cut"]:
+                additional_args["-parallel"] = "1"
+            self._time_run(additional_args)
+        else:
+            log.warning("no quality/time run values provided")
 
-        log.info("completed batch run.")
-
-    def _run_quality_comparison(self, compare_type: str, key: str,
-                                values: typing.List[str],
-                                additional_args: typing.Dict[str, str]):
-        log.info(f"starting quality comparison runs for {compare_type}...")
-        counter = 0
+    def _quality_run(self, additional_args: typing.Dict[str, str]):
+        log.info(f"starting {self.config.prefix} quality run for instance "
+                 f"{self.config.instance}...")
         args = self._defaults.copy()
-        for k,v in additional_args.items():
+        for k, v in additional_args.items():
             args[k] = v
-        for name in self.config.names:
-            args["-name"] = name
-            for value in values:
-                args[key] = value
-                cmd = [c for c in self._base_cmd]
-                for k,v in args.items():
-                    cmd.extend([k,v])
+        args["-name"] = self.config.instance
+        args[self.config.key] = self.config.quality_run
+        cmd = [c for c in self._base_cmd]
+        for k, v in args.items():
+            cmd.extend([k, v])
+        generate_all_results(cmd, self._models, self.config.prefix)
+        clean_delay_files()
+        log.info(f"completed {self.config.prefix} quality run for instance "
+                 f"{self.config.instance}...")
 
-                prefix = f"{compare_type}_{counter}"
-                generate_all_results(cmd, self._models, prefix)
+    def _time_run(self, additional_args: typing.Dict[str, str]):
+        log.info(f"starting {self.config.prefix} time run for instance "
+                 f"{self.config.instance}...")
+        args = self._defaults.copy()
+        if self.config.key in args:
+            del args[self.config.key]
+        for k, v in additional_args.items():
+            args[k] = v
+
+        args["-name"] = self.config.instance
+        args["-type"] = "benders"
+
+        counter = 0
+        for i in range(5):
+            cmd = [c for c in self._base_cmd]
+            for k, v in args.items():
+                cmd.extend([k, v])
+
+            generate_delays(cmd)
+            for value in self.config.time_run:
+                run_cmd = [c for c in cmd]
+                run_cmd.extend([
+                    "-parseDelays",
+                    "-model", "benders",
+                    self.config.key, value,
+                    "-output", f"solution/{self.config.prefix}_{counter}.yaml"
+                ])
+                subprocess.check_call(run_cmd)
                 counter += 1
-        clean_delay_files()
-        log.info(f"completed budget comparison runs for {compare_type}.")
-
-    def _run_time_comparison(self, compare_type: str, key: str,
-                             values: typing.List[str],
-                             additional_args: typing.Dict[str, str]):
-        log.info(f"starting time comparison runs for {compare_type}...")
-        counter = 0
-        args = self._defaults.copy()
-        if key in args:
-            del args[key]
-        for k,v in additional_args.items():
-            args[k] = v
-        for name in self.config.names:
-            args["-name"] = name
-            args["-type"] = "benders"
-            for _ in range(5):
-                cmd = [c for c in self._base_cmd]
-                for k,v in args.items():
-                    args.extend([k,v])
-                generate_delays(cmd)
-
-                for value in values:
-                    run_cmd = [c for c in cmd]
-                    run_cmd.extend([
-                        "-parseDelays",
-                        "-model", "benders",
-                        key, value,
-                        "-output", f"solution/{name}_{counter}.yaml"
-                    ])
-                    subprocess.check_call(run_cmd)
-                    log.info(f"finished time comparison run for {run_cmd}")
-                    counter += 1
 
         clean_delay_files()
-        log.info(f"completed time comparison runs for {compare_type}.")
+        log.info(f"completed {self.config.prefix} time run for instance "
+                 f"{self.config.instance}...")
 
 
 def guess_cplex_library_path() -> str:
@@ -255,34 +221,30 @@ def handle_command_line() -> Config:
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    run_type_dict = {
-        "a": RunType.All,
-        "b": RunType.Budget,
-        "c": RunType.ColumnCaching,
-        # "e": RunType.ExpectedExcess,
-        "m": RunType.Mean,
-        "p": RunType.Parallel,
-        "q": RunType.Quality,
-        "t": RunType.ColumnComparison,
-        "u": RunType.CutComparison
-    }
+    parser.add_argument("-i", "--instance", type=str,
+                        help="name of instance", default="s6")
+
+    parser.add_argument("-k", "--key", type=str,
+                        help="JAR arg name", required=True)
 
     root = get_root()
-    parser.add_argument("-r", "--run_type", type=str,
-                        choices=list(run_type_dict.keys()), help="type of batch run",
-                        default="b")
-
     default_path = os.path.join(root, "data")
     parser.add_argument("-p", "--path", type=str, default=default_path,
                         help="path to folder with xml files")
 
-    default_names: typing.List[str] = [f"s{i}" for i in range(1, 7)]
-    parser.add_argument("-n", "--names", type=str, nargs="+", default=default_names,
-                        help="names of instances to run")
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("-q", "--quality_run", type=str,
+                       help="value to use for quality comparison run")
+    group.add_argument("-t", "--time_run", type=str, nargs="+", default=[],
+                       help="values to use for time comparison run")
+
+    parser.add_argument("-x", "--prefix", type=str, required=True,
+                        help="prefix to use for output files")
 
     args_dict = vars(parser.parse_args())
+    key = args_dict["key"]
+    args_dict["key"] = f"-{key}"
     args_dict["cplex_lib_path"] = guess_cplex_library_path()
-    args_dict["run_type"] = run_type_dict[args_dict["run_type"]]
     args_dict["jar_path"] = os.path.join(
         root, "build", "libs", "stochastic_uber.jar")
     return Config(**args_dict)
